3e929082d45af6927f2c01c99a111b8ae4473454
devdoni <devdoni1005@gmail.com>
2024-11-18 16:06:26 +0900

    first commit

---
+++ src/main/java/com/office/springde/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.office.springde.config;
+
+public class AppConfig {
+
+    final static public String SERVER_NAME_LOCAL = "local";
+    final static public String SERVER_NAME_DEV = "dev";
+    final static public String SERVER_NAME_PROD = "prod";
+}
